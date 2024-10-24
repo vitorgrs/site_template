3b8a884f57a66b6826d81853a5e7aafb7a23e80c
Pedro Fonseca <pedro.fonseca@nca.ufma.br>
2024-10-24 08:42:20 -0300

    [Pedro Fonseca] fix: ajustando scripts das pag. de formulários

--- assets/js/formMembros.js
+++ assets/js/formMembros.js
@@ -6,21 +6,19 @@ if (formMembro){
     formMembro.addEventListener("submit", (e) =>{
     e.preventDefault();
     formData = new FormData(formMembro);
-    formData.append("nome_membro", document.getElementById("nomeMembro").value);
-    console.log(formData);
-
+    
     const requestOptions = {
         method: 'POST',
         body: formData,
     };
 
 
-    fetch(apiMembroUrl, requestOptions)
-    .then(response => {
-            if (!response.ok){
-                throw new Error('Erro ao chamar API');
-            }
-            return response.json();
+        fetch(apiMembroUrl, requestOptions)
+        .then(response => {
+                if (!response.ok){
+                    throw new Error('Erro ao chamar API');
+                }
+                return response.json();
         })
     })
 }
